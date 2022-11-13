c20466b35a307680079429d3a8a8ef07849ab918
Raypcte <rayanevalak@gmail.com>
2022-11-13 12:26:23 -0300

    iniciando

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const productsRouter = require('./route');
 
 const app = express();
+
+response.send();
+
+app.use('/products', productsRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
