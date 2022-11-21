5c95194faab3033225b5336ff6fe4cfbf8dfd3f4
Raypcte <rayanevalak@gmail.com>
2022-11-20 21:30:50 -0300

    req4

--- src/route.js
+++ src/route.js
@@ -6,6 +6,6 @@ const productsRouter = express.Router();
 
 productsRouter.get('/products', productController.getProducts);
 productsRouter.get('/products/:id', mid.midProduct, productController.getProductsById);
-productsRouter.post('/products', productController.createNewProducts);
+productsRouter.post('/products', mid.validationName, productController.createNewProducts);
 
 module.exports = productsRouter;
